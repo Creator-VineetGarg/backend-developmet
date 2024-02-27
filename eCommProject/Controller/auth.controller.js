74aa2102eaafdd44c1173616cc5dacff2c764dba
Vineet-Garg <contact.vineetgarg@gmail.com>
2024-02-27 20:20:34 +0530

    Update

--- eCommProject/Controller/auth.controller.js
+++ eCommProject/Controller/auth.controller.js
@@ -1,9 +1,11 @@
 /**
  * I need to write the controller | logic to register a user
  */
-
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const user_model = require("../Models/user.model")
+const Secret = require('../Configs/auth.config')
+
 exports.signup = async (req, res) => {
     /**
      * Logic to create the user
@@ -44,4 +46,37 @@ exports.signup = async (req, res) => {
     }
 
     // 3. Return the response back to the user
+}
+
+exports.signin = async(req, res)=>{
+
+    // Checks if the user id is present in the database
+    const user = await user_model.findOne({userId : req.body.userId}) 
+
+    if(user == null){
+        res.status(400).send({
+            message : "User Id passed is not a valid user"
+        })
+    }
+
+    // Checks if the password is correct
+    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password)
+    if(!isPasswordValid){
+        res.status(400).send({
+            message : "Wrong Password passed!!!"
+        })
+    }
+    
+    // Using JWT we will create the access token with a given TTL and return it.
+    const token = jwt.sign({id : user.userId}, Secret.secret, {
+        expiresIn : 120
+    })
+
+    res.status(200).send({
+        name : user.name,
+        userId : user.userId,
+        email : user.email,
+        userType : user.userType,
+        accessToken : token
+    })
 }
