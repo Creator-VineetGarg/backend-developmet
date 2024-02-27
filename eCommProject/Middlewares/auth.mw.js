2257a2dacd8e92481d3fdb79124311a61d43e9d8
Vineet-Garg <contact.vineetgarg@gmail.com>
2024-02-27 18:01:08 +0530

    Update

---
+++ eCommProject/Middlewares/auth.mw.js
@@ -0,0 +1,61 @@
+/**
+ * Create a mw will check if the request body is proper and correct
+ */
+
+const user_model = require('../Models/user.model')
+
+const verifySignupBody = async(req, res, next)=>{
+    try {
+        // Check for the name
+        if(!req.body.name){
+            return res.status(400).send({
+                message : "Failed!!! Name was not Provided in request body"
+            })
+        }
+
+        // check for the email
+        if(!req.body.email){
+            return res.status(400).send({
+                message : "Failed!!! Email was not Provided in request body"
+            })
+        }
+
+        // Check for the userId
+        if(!req.body.userId){
+            return res.status(400).send({
+                message : "Failed!!! userId was not Provided in request body"
+            })
+        }
+
+        // Check for existing email or not
+        const mail = await user_model.findOne({email : req.body.email})
+
+        if(mail){
+            res.status(400).send({
+                message : "Failed!!! Email has been already defined in request body"
+            })
+        }
+
+        // Check for existing userID or not
+        const user = await user_model.findOne({userId : req.body.userId})
+        
+        if(user){
+            return res.status(400).send({
+                message : "Failed!!! UserId was already defined in request body"
+            })
+        }
+
+        next()
+
+    }catch(err){
+        console.log("Error while validating the request object body", err)
+        res.status(500).send({
+            message: 'Error while validating'
+        })
+
+    }
+}
+
+module.exports = {
+    verifySignupBody : verifySignupBody
+}
--- eCommProject/Router/auth.route.js
+++ eCommProject/Router/auth.route.js
@@ -1,8 +1,9 @@
 /**
  * Post call
  */
+const authMW = require('../Middlewares/auth.mw')
 const authController = require('../Controller/auth.controller');
 
 module.exports = (app)=>{
-    app.post("/ecomm/api/v1/auth/signup", authController.signup)
+    app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignupBody],authController.signup)
 }
