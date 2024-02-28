e3093cd2023e6e05a99878c96777d4a0075500c5
Vineet-Garg <contact.vineetgarg@gmail.com>
2024-02-28 12:40:56 +0530

    Update

--- eCommProject/Controller/category.controller.js
+++ eCommProject/Controller/category.controller.js
@@ -2,6 +2,10 @@
  * Defining controller for creating the category
  */
 const category_model = require('../Models/category.model')
+
+/**
+ * For POST method
+ */
 exports.createNewCategory = async(req, res)=>{
     // read the response body
     // const request_body = req.body
@@ -25,4 +29,87 @@ exports.createNewCategory = async(req, res)=>{
     }
 
     // Return the response of the created category
+}
+
+/**
+ * For GET method
+ */
+exports.fetchCategory = async(req, res)=>{
+
+    try{
+
+        const categories = await category_model.find()
+        // return it
+        return res.status(201).send(categories)
+
+    }catch(err){
+        return res.status(500).send({
+            message : "Error in fetching the categories"
+        })
+    }
+    
+}
+
+/**
+ * For PUT Method
+ */
+exports.editCategory = async(req, res)=>{
+
+    try{
+
+        // If the Category Doesnt' present then 
+        const existingCategory = await category_model.findOne({ name: req.body.name });
+
+        if (!existingCategory) {
+            return res.status(400).send({
+                message: "Category Not found"
+            });
+        }
+
+        // Then Update
+        await category_model.findOneAndUpdate({name : req.body.name}, {$set: {
+            name : req.body.newName,
+            description  : req.body.description
+        }})
+
+        
+        // Returning the New Category
+        const NewCategory = await category_model.findOne({name : req.body.newName})
+        return res.status(201).send(NewCategory)
+
+    }catch(err){
+        return res.status(500).send({
+            message : "Error in editing the category"
+        })
+    }
+}
+
+
+/**
+ * For DELETE Method
+ */
+exports.deleteCategory = async(req, res)=>{
+    try{
+    
+        // if category is not found
+        const existingCategory = await category_model.findOne({ name: req.body.name });
+
+        if (!existingCategory) {
+            return res.status(400).send({
+                message: "Category Not found"
+            });
+        }
+        
+        // then Delete
+        await category_model.deleteOne({name: req.body.name})
+
+        // Now it returns the categories left
+        const categories = await category_model.find()
+        return res.status(201).send(categories)
+    
+    }catch(err){
+        return res.status(500).send({
+            message : "Error in deleting the category"
+        })
+    }
 }
--- eCommProject/Router/category.routes.js
+++ eCommProject/Router/category.routes.js
@@ -2,5 +2,8 @@ const category_routes = require('../Controller/category.controller')
 const authMw = require('../Middlewares/auth.mw')
 module.exports = (app)=>{
     app.post("/ecomm/api/v1/categories", category_routes.createNewCategory)
+    app.get("/ecomm/api/v1/getCategories", category_routes.fetchCategory)
+    app.put("/ecomm/api/v1/editCategories", category_routes.editCategory)
+    app.delete("/ecomm/api/v1/delCategories", category_routes.deleteCategory)
     // app.post("/ecomm/api/v1/categories",[authMw.verifyToken], category_routes.createNewCategory)
 }
